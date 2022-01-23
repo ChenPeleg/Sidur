b9de840df06b248ce9cf4e5f69fa845ec1f984fc
Chen <Chenpeleg>
2022-01-23 15:25:54 +0200

    before installing router

--- src/components/buttons/styled-toggle-button.tsx
+++ src/components/buttons/styled-toggle-button.tsx
@@ -5,6 +5,9 @@ import {ToggleButtonGroup} from '@mui/material';
 export const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({theme: _theme}) => ({
     '& .MuiToggleButtonGroup-grouped': {
         backgroundColor: 'rgba(255,255,255,0.11)',
+        '&:hover': {
+            backgroundColor: 'rgba(255,255,255,0.21)',
+        },
         // margin: _theme.spacing(0.5),
         // border: 0,
         '&.Mui-disabled': {
@@ -14,6 +17,7 @@ export const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({theme: _theme
             backgroundColor: 'rgb(187,185,249)',
             // border: 0,
         },
+
         '&:not(:first-of-type)': {
             // borderRadius: _theme.shape.borderRadius,
         },
