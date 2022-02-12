b415f3116625a55682d131b1a4e4aebaa3950b55
Chen <Chenpeleg>
2022-02-12 16:37:21 +0200

    added replace drive function 2

--- src/store/sketch-drive.reducer.ts
+++ src/store/sketch-drive.reducer.ts
@@ -5,6 +5,8 @@ import {IAction, SidurStore} from './store.types';
 import {DriveModel, SketchModel, VehicleScheduleModel} from '../models/Sketch.model';
 import {OrderModel} from '../models/Order.model';
 import {StoreUtils} from './store-utils';
+import {Utils} from '../services/utils';
+import {LanguageUtilities} from '../services/language-utilities';
 
 export type SketchDriveReducerFunctions =
     ActionsTypes.DELETE_SKETCH_DRIVE
@@ -33,7 +35,8 @@ export const SketchDriveReducer: Record<SketchDriveReducerFunctions, (state: Sid
                     } else {
                         return d
                     }
-                })
+                });
+                sortVehicleByStartHour(relevantVehicle);
             }
 
 
@@ -62,6 +65,7 @@ export const SketchDriveReducer: Record<SketchDriveReducerFunctions, (state: Sid
                         return d
                     }
                 })
+                sortVehicleByStartHour(relevantVehicle)
             }
             if (pendingOrdersToPassToAssigned !== null) {
                 const implementedOrders: string [] = pendingOrdersToPassToAssigned as string [];
@@ -81,29 +85,43 @@ export const SketchDriveReducer: Record<SketchDriveReducerFunctions, (state: Sid
         let newState = {...state}
         const sketchDriveToReplace: DriveModel = action.payload.value;
         const SketchIdInEdit = newState.sessionState.SketchIdInEdit;
+        const pendingOrderId = newState.sessionState.pendingOrderIdInEdit;
         newState.sessionState.pendingOrderInEditAction = null;
         newState.sessionState.pendingOrderInEditActionSelectDrives = null;
         const sketchObj: SketchModel | undefined = newState.sketches.find((record: SketchModel) => record.id === SketchIdInEdit);
+        const pendingOrder = sketchObj?.unassignedOrders.find(o => o.id === pendingOrderId);
 
-        if (sketchObj !== undefined) {
+
+        if (sketchObj && pendingOrder) {
+            const newDriveId = getNewDriveIdFromSketch(sketchObj);
+            const newDriveToInsert: DriveModel = {
+                ...pendingOrder,
+                id: newDriveId,
+                implementsOrders: [pendingOrder.id],
+                description: LanguageUtilities.buildBriefText(pendingOrder, newState.Locations).driverAndLocation
+            }
             const vehicleId = getVehicleIdFromDriveId(state, sketchDriveToReplace.id);
             const relevantVehicle = sketchObj.vehicleSchedules.find(v => v.id === vehicleId);
-            let pendingOrdersToPassToAssigned: string [] | null = null;
+            let assignedOrdersPassToPending: string [] | null = null;
             if (relevantVehicle) {
                 relevantVehicle.drives = relevantVehicle.drives.map((d: DriveModel) => {
                     if (d.id === sketchDriveToReplace.id) {
-                        pendingOrdersToPassToAssigned = d.implementsOrders
-                        return sketchDriveToReplace
+                        assignedOrdersPassToPending = d.implementsOrders
+                        return newDriveToInsert
                     } else {
                         return d
                     }
                 })
+                sortVehicleByStartHour(relevantVehicle)
             }
-            if (pendingOrdersToPassToAssigned !== null) {
-                const implementedOrders: string [] = pendingOrdersToPassToAssigned as string [];
-                const ordersToMoveToAssigned: OrderModel [] = sketchObj.unassignedOrders.filter((o: OrderModel) => implementedOrders.includes(o.id))
-                sketchObj.assignedOrders = sketchObj.assignedOrders.concat(ordersToMoveToAssigned);
-                sketchObj.unassignedOrders = sketchObj.unassignedOrders.filter((o: OrderModel) => !implementedOrders.includes(o.id))
+
+            sketchObj.unassignedOrders = sketchObj.unassignedOrders.filter((o: OrderModel) => o.id !== pendingOrderId);
+
+            if (assignedOrdersPassToPending !== null) {
+                const implementedOrdersFromReplacedDrive: string [] = assignedOrdersPassToPending as string [];
+                const ordersToMoveToPending: OrderModel [] = sketchObj.assignedOrders.filter((o: OrderModel) => implementedOrdersFromReplacedDrive.includes(o.id))
+                sketchObj.unassignedOrders = sketchObj.unassignedOrders.concat(ordersToMoveToPending);
+                sketchObj.assignedOrders = sketchObj.assignedOrders.filter((o: OrderModel) => !implementedOrdersFromReplacedDrive.includes(o.id))
 
 
             }
@@ -138,6 +156,7 @@ export const SketchDriveReducer: Record<SketchDriveReducerFunctions, (state: Sid
                         return d
                     }
                 })
+                sortVehicleByStartHour(relevantVehicle)
             }
             let OrderToMoveToUnassinged: OrderModel | undefined = sketchObj.assignedOrders.find(o => o.id === orderIdToRemove);
             if (OrderToMoveToUnassinged) {
@@ -173,7 +192,8 @@ export const SketchDriveReducer: Record<SketchDriveReducerFunctions, (state: Sid
                         return d
                     }
                 });
-                relevantVehicle.drives = newDrives.filter(d => d) as DriveModel[]
+                relevantVehicle.drives = newDrives.filter(d => d) as DriveModel[];
+                sortVehicleByStartHour(relevantVehicle)
             }
 
 
@@ -201,6 +221,28 @@ const getVehicleIdFromDriveId = (state: SidurStore, driveId: string): string => 
         })
     })
     return vehicleId
+
+}
+const getNewDriveIdFromSketch = (sketch: SketchModel): string => {
+    const allDriveIds: string [] = ['1'];
+    sketch.vehicleSchedules.forEach(v => {
+        v.drives.forEach(d => {
+            allDriveIds.push(d.id)
+        })
+    })
+    return Utils.getNextId(allDriveIds)
+
+}
+const sortVehicleByStartHour = (vehicle: VehicleScheduleModel): VehicleScheduleModel => {
+
+    vehicle.drives.sort((aDrive, bDrive) => {
+        const a = Utils.hourTextToDecimal(aDrive.startHour);
+        const b = Utils.hourTextToDecimal(bDrive.startHour);
+
+        return a < b ? -1 : b < a ? 1 : 0
+
+    })
+    return vehicle
 
 }
 
