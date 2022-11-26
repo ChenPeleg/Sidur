50f55134b2faf32d138571228b5b59443f974a64
chenp <chenpeleg@gmail.com>
2022-11-26 20:49:22 +0200

    changed files in source

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,10 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import {configure} from 'enzyme';
-import React from 'react'
+import "@testing-library/jest-dom";
+import React from "react";
 
-React.useLayoutEffect = React.useEffect
-configure({adapter: new Adapter()})
+React.useLayoutEffect = React.useEffect;
