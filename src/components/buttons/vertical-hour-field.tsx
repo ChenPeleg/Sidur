48678dfe8b7c55a533bd54586b74a5ff450b93b8
chenp <chenpeleg@gmail.com>
2023-03-10 21:08:30 +0200

    fixed position scaling

--- src/components/buttons/vertical-hour-field.tsx
+++ src/components/buttons/vertical-hour-field.tsx
@@ -21,7 +21,8 @@ interface VerticalHourFieldProps {
 }
 
 const formatHourLabel = (hourInNumber: number) =>
-    Utils.DecimalTimeToHourText(hourInNumber)
+    `${Utils.DecimalTimeToHourText(hourInNumber)}`;
+
 export const VerticalHourField = (
     props: VerticalHourFieldProps
 ) => {
