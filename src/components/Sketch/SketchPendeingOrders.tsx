dbe850c23e68a29b9a94261824df51235b556db6
chenp <chenpeleg@gmail.com>
2023-03-11 20:22:54 +0200

    added pending orders folding

--- src/components/Sketch/SketchPendeingOrders.tsx
+++ src/components/Sketch/SketchPendeingOrders.tsx
@@ -1,71 +1,82 @@
 import React, {useState} from 'react'
 import {Box, SxProps} from '@mui/system';
 import {useSelector} from 'react-redux';
-import {Typography} from '@mui/material';
+import {Badge, Collapse, Typography} from '@mui/material';
 import {OrderModel} from '../../models/Order.model';
 import {translations} from '../../services/translations';
 import {SketchPendingOrder} from './SketchPendingOrder';
 import {SidurStore} from '../../store/store.types';
 import IconButton from '@mui/material/IconButton';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import {ArrowBack, CarRentalTwoTone, Keyboard, KeyboardArrowLeft, KeyboardArrowRight} from '@mui/icons-material';
+import {KeyboardArrowLeft, KeyboardArrowRight} from '@mui/icons-material';
+import {StyledBadge} from '../Icons/styled-badge';
 
 
 interface sketchPendingOrdersProps {
     pendingOrders: OrderModel[],
 }
 
-const foldingButtonSX : SxProps = {
-    backgroundColor : 'rgba(240,240,240,0.7)',
+const foldingButtonSX: SxProps = {
+    backgroundColor: 'rgba(240,240,240,0.7)',
     borderRadius: '50%',
     boxShadow: `rgb(9 30 66 / 16%) 0px 0px 0px 1px, rgb(9 30 66 / 16%) 0px 2px 4px 1`
 };
 export const SketchPendingOrders = (props: sketchPendingOrdersProps) => {
 
     const [isPendingOrdersFolded, setIsPendingOrdersFolded] = useState(false);
-    const handleOpenClosePendingOrders =() => {
+    const handleOpenClosePendingOrders = () => {
         setIsPendingOrdersFolded(!isPendingOrdersFolded)
     }
     const pendingOrderInEdit = useSelector((state: SidurStore) => state.sessionState.pendingOrderIdInEdit);
 
-    return (<Box id={'pending-order-hide-container'} sx={{    display: 'flex',
-            flexDirection: 'row',}}>
-
-
-        <Box id={'pending-order-container'} sx={{
-            backgroundColor : 'rgba(255,255,255,0.1)',
-            m: '0.2em',
-            mb: '0.3em',
-            minHeight: '80vh',
-            minWidth: '30vw',
-            height : '100%',
+    return (<Box id={'pending-order-hide-container'} sx={{
             display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'stretch',
-            justifyContent: 'start',
+            flexDirection: 'row',
+            // mt : '6px',
+            pr : '10px'
         }}>
-            <Typography variant={'h6'}> &nbsp;  {translations.PendingOrders} </Typography>
 
-            {(props.pendingOrders || []).map((order: OrderModel) => {
-                return <SketchPendingOrder isInEdit={pendingOrderInEdit === order.id} key={order.id} order={order}/>
-            })}
-        </Box>
-            <Box id={'close-button-container'} sx={{height: '30px', mr : '-12px'}}>
+        <Collapse orientation="horizontal" in={!isPendingOrdersFolded}>
 
 
-            <IconButton
-                size="medium"
-                edge="end"
-                aria-label="account of current user"
-                onClick={handleOpenClosePendingOrders}
-                color="inherit"
-            >
-                {isPendingOrdersFolded ?   <KeyboardArrowLeft   fontSize={'medium'}
-                                                                sx={foldingButtonSX} /> :
-                    <KeyboardArrowRight   fontSize={'medium'}
-                                          sx={foldingButtonSX} /> }
+            <Box id={'pending-order-container'} sx={{
+                // backgroundColor: 'rgba(255,255,255,0.1)',
+                m: '0.2em',
+                mb: '0.3em',
+                minHeight: '80vh',
+                minWidth: '30vw',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'stretch',
+                justifyContent: 'start',
+            }}>
+                <Typography variant={'h6'}> &nbsp;  {translations.PendingOrders} </Typography>
 
-            </IconButton>
+                {(props.pendingOrders || []).map((order: OrderModel) => {
+                    return <SketchPendingOrder isInEdit={pendingOrderInEdit === order.id} key={order.id} order={order}/>
+                })}
+            </Box>
+        </Collapse>
+            <Box id={'close-button-container'} sx={{
+                height: '30px',
+                mr: '-12px'
+            }}>
+                <IconButton
+                    size="medium"
+                    edge="end"
+                    aria-label="account of current user"
+                    onClick={handleOpenClosePendingOrders}
+                    color="inherit"
+                >
+                    {isPendingOrdersFolded ? <StyledBadge  badgeContent={props.pendingOrders.length} color="secondary">
+                            <KeyboardArrowLeft fontSize={'medium'}
+                                               sx={foldingButtonSX}/>
+                        </StyledBadge>
+                       :
+                        <KeyboardArrowRight fontSize={'medium'}
+                                            sx={foldingButtonSX}/>}
+
+                </IconButton>
             </Box>
         </Box>
     )
