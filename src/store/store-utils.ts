57c79ff48b969a460c0a42b3b32b17c2c5031b7a
Chen Peleg <chenpeleg@gmail.com>
2023-12-07 10:28:55 +0200

    added another function 3

--- src/store/store-utils.ts
+++ src/store/store-utils.ts
@@ -1,109 +1,147 @@
-import {AppConstants, RecordBriefModel, SaveDataModel, SessionModel, SidurRecord, SidurStore, TypeOfRecord} from './store.types';
-import {SaveLoadService} from '../services/save-load.service';
-import {hashFunction} from '../services/hash-function';
-import {CloneUtil} from '../services/clone-utility';
-import {ActionsTypes} from './types.actions';
+import {
+    AppConstants,
+    RecordBriefModel,
+    SaveDataModel,
+    SessionModel,
+    SidurRecord,
+    SidurStore,
+    TypeOfRecord,
+} from "./store.types";
+import { SaveLoadService } from "../services/save-load.service";
+import { hashFunction } from "../services/hash-function";
+import { CloneUtil } from "../services/clone-utility";
+import { ActionsTypes } from "./types.actions";
 import { SketchModel } from "../models/Sketch.model";
 import { arrangeSketchInCarColumns } from "./arrangeSketchInCarColumns";
-
+import { VehicleModel } from "../models/Vehicle.model";
 
 export const StoreUtils = {
-    buildCSVFileFromSketch: (sketchObj: SketchModel, preferences: any): string => {
-        return arrangeSketchInCarColumns(sketchObj, preferences)
+    buildCSVFileFromSketch: (
+        sketchObj: SketchModel,
+        vehicles: VehicleModel[],
+        preferences: any
+    ): string => {
+        return arrangeSketchInCarColumns(sketchObj, vehicles, preferences);
     },
     removeIdPrefix: (id: string): string => {
-        const replaceIdsNames: RegExp = new RegExp(AppConstants.ArchiveIdPrefix + '|' + AppConstants.deleteIdPrefix, 'g');
-        return id.replace(replaceIdsNames, '')
+        const replaceIdsNames: RegExp = new RegExp(
+            AppConstants.ArchiveIdPrefix + "|" + AppConstants.deleteIdPrefix,
+            "g"
+        );
+        return id.replace(replaceIdsNames, "");
     },
     HandleReducerSaveToLocalStorage: (state: SidurStore): void => {
-        const saveObj: SaveDataModel = StoreUtils.buildSaveDataModel(state, 'chen', 'chen')
+        const saveObj: SaveDataModel = StoreUtils.buildSaveDataModel(
+            state,
+            "chen",
+            "chen"
+        );
         SaveLoadService.saveToLocalStorage(saveObj);
     },
     updateRecordBrief: (state: SidurStore): SidurStore => {
-        const newState = {...state}
-        const allSiduresVisiable = newState.sidurCollection.concat(newState.sidurArchive);
-        const allLocationGroups = [...newState.LocationGroups]
-        const sidurRecords: RecordBriefModel[] = allSiduresVisiable.map(s => {
+        const newState = { ...state };
+        const allSiduresVisiable = newState.sidurCollection.concat(
+            newState.sidurArchive
+        );
+        const allLocationGroups = [...newState.LocationGroups];
+        const sidurRecords: RecordBriefModel[] = allSiduresVisiable.map((s) => {
             const newSidurRecord: RecordBriefModel = {
                 dbId: s.dbId,
                 id: s.id,
                 locationGroupOrSidurId: s.locationGroupIdForSidur,
                 name: s.Name,
-                typeOfRecord: TypeOfRecord.Sidur
-            }
-            return newSidurRecord
-        })
-        const locationRecords: RecordBriefModel[] = allLocationGroups.map(s => {
-            const newSidurRecord: RecordBriefModel = {
-                dbId: s.dbId,
-                id: s.id,
-                locationGroupOrSidurId: s.id,
-                name: s.name,
-                typeOfRecord: TypeOfRecord.LocationGroup
-            }
-            return newSidurRecord
-        })
-
-        newState.recordBriefs = sidurRecords.concat(locationRecords)
-        return newState
-    },
-
-    UpdateSidurCollectionWithCurrenSidur: (state: SidurStore): SidurRecord[] => {
-        const newState = {...state}
-        const sidurId = newState.sidurId;
-        const updatedOrders = newState.orders.map(o => ({...o}));
-        const updatedVehicles = newState.vehicles.map(o => ({...o}));
-        const updatedDeletedOrders = newState.deletedOrders.map(o => ({...o}));
-        const updatedSketches = newState.sketches.map(o => ({...o}))
-        newState.sidurCollection = newState.sidurCollection.map((sidur: SidurRecord) => {
-            if (sidur.id === sidurId) {
-                const updatedSidur = {...sidur};
-                updatedSidur.orders = updatedOrders;
-                updatedSidur.vehicles = updatedVehicles;
-                updatedSidur.sketches = updatedSketches;
-                updatedSidur.deletedOrders = updatedDeletedOrders;
-                return updatedSidur
-            } else {
-                return sidur
-            }
+                typeOfRecord: TypeOfRecord.Sidur,
+            };
+            return newSidurRecord;
         });
-        return newState.sidurCollection
+        const locationRecords: RecordBriefModel[] = allLocationGroups.map(
+            (s) => {
+                const newSidurRecord: RecordBriefModel = {
+                    dbId: s.dbId,
+                    id: s.id,
+                    locationGroupOrSidurId: s.id,
+                    name: s.name,
+                    typeOfRecord: TypeOfRecord.LocationGroup,
+                };
+                return newSidurRecord;
+            }
+        );
+
+        newState.recordBriefs = sidurRecords.concat(locationRecords);
+        return newState;
     },
-    buildSaveDataModel: (state: SidurStore, userName: string = 'chen', userId: string = 'chen'): SaveDataModel => {
-        const hash = hashFunction(JSON.stringify(state))
+
+    UpdateSidurCollectionWithCurrenSidur: (
+        state: SidurStore
+    ): SidurRecord[] => {
+        const newState = { ...state };
+        const sidurId = newState.sidurId;
+        const updatedOrders = newState.orders.map((o) => ({ ...o }));
+        const updatedVehicles = newState.vehicles.map((o) => ({ ...o }));
+        const updatedDeletedOrders = newState.deletedOrders.map((o) => ({
+            ...o,
+        }));
+        const updatedSketches = newState.sketches.map((o) => ({ ...o }));
+        newState.sidurCollection = newState.sidurCollection.map(
+            (sidur: SidurRecord) => {
+                if (sidur.id === sidurId) {
+                    const updatedSidur = { ...sidur };
+                    updatedSidur.orders = updatedOrders;
+                    updatedSidur.vehicles = updatedVehicles;
+                    updatedSidur.sketches = updatedSketches;
+                    updatedSidur.deletedOrders = updatedDeletedOrders;
+                    return updatedSidur;
+                } else {
+                    return sidur;
+                }
+            }
+        );
+        return newState.sidurCollection;
+    },
+    buildSaveDataModel: (
+        state: SidurStore,
+        userName: string = "chen",
+        userId: string = "chen"
+    ): SaveDataModel => {
+        const hash = hashFunction(JSON.stringify(state));
         return {
-            userName: 'chen',
-            userId: 'chen',
+            userName: "chen",
+            userId: "chen",
             savedStore: state,
             timeStamp: SaveLoadService.getTimeStampFromDate(),
-            hash: hash.toString()
-        }
-
+            hash: hash.toString(),
+        };
     },
     updateSidurRecordWithSketchChanges(state: SidurStore): SidurStore {
-        const newState = {...state};
-        const thisSidurInCollection: SidurRecord | undefined = newState.sidurCollection.find((sidur: SidurRecord) => sidur.id === newState.sidurId);
-
+        const newState = { ...state };
+        const thisSidurInCollection: SidurRecord | undefined =
+            newState.sidurCollection.find(
+                (sidur: SidurRecord) => sidur.id === newState.sidurId
+            );
 
         if (thisSidurInCollection) {
-            thisSidurInCollection.sketches = newState.sketches.map(s => CloneUtil.deepCloneSketch(s))
+            thisSidurInCollection.sketches = newState.sketches.map((s) =>
+                CloneUtil.deepCloneSketch(s)
+            );
         }
 
-        return newState
-
+        return newState;
     },
-    shieldAnimationBeforeDispatch(dispatchingAction: () => void, dispatchFunct: any, delay: number = 600) {
+    shieldAnimationBeforeDispatch(
+        dispatchingAction: () => void,
+        dispatchFunct: any,
+        delay: number = 600
+    ) {
         const callArguments = {
             type: ActionsTypes.START_LOADING_ANIMATION,
             payload: {
                 value: null,
-            }
-        }
-        dispatchFunct(callArguments)
-        setTimeout(_ => {
+            },
+        };
+        dispatchFunct(callArguments);
+        setTimeout((_) => {
             dispatchingAction();
-        }, delay)
-
+        }, delay);
     },
 
     defaultSessionState(): SessionModel {
@@ -120,13 +158,13 @@ export const StoreUtils = {
             pendingOrderIdInEdit: null,
             routeIdInEdit: null,
             transportIdInEdit: null,
-            openDialog : null,
-            importSheetCheckStatus : false
-        }
-        return defaultSession
+            openDialog: null,
+            importSheetCheckStatus: false,
+        };
+        return defaultSession;
     },
     abortSessionPendingOrderState(state: SidurStore) {
         state.sessionState.pendingOrderInEditActionSelectDrives = [];
         state.sessionState.pendingOrderInEditAction = null;
-    }
-}
+    },
+};
