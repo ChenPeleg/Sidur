25438703e0d1e332298bed0a679bc30c359c55bf
Chen Peleg <chenpeleg@gmail.com>
2023-12-07 10:21:37 +0200

    added another function 2

--- src/store/arrangeSketchInCarColumns.ts
+++ src/store/arrangeSketchInCarColumns.ts
@@ -1,5 +1,46 @@
-import { SketchModel } from "../models/Sketch.model";
+import { SketchModel, VehicleScheduleModel } from "../models/Sketch.model";
 
-export const arrangeSketchInCarColumns = (sketchObj: SketchModel, preferences: any): string => {
-    return ',,,,,,,,,,,,,'
-}
+type VehicleSketchColumn = {
+    vehicleSchedule: VehicleScheduleModel;
+    numberOfColumns: number;
+    position: number;
+};
+
+const NUMBER_OF_ROWS_FOR_HEADER = 2;
+const NUMBER_OF_COLUMNS_FOR_VEHICLE = 3;
+
+const calculateNumberOfRows = (sketchObj: SketchModel): number => {
+    const drivesPerVehicle = sketchObj.vehicleSchedules.map(
+        (v) => v.drives.length
+    );
+    return (
+        drivesPerVehicle.reduce((a, b) => Math.max(a, b), 0) +
+        NUMBER_OF_ROWS_FOR_HEADER
+    );
+};
+
+const vehicleSketchModelToVehicleSketchColumn = (
+    vehicleSchedule: VehicleScheduleModel,
+    numberOfColumns: number
+) => {
+    return {
+        vehicleSchedule,
+        numberOfColumns: numberOfColumns,
+        position: 0,
+    };
+};
+
+export const arrangeSketchInCarColumns = (
+    sketchObj: SketchModel,
+    preferences: any
+): string => {
+    const numberOfRows = calculateNumberOfRows(sketchObj);
+    const vehicleSketchColumns = sketchObj.vehicleSchedules.map((v) =>
+        vehicleSketchModelToVehicleSketchColumn(
+            v,
+            NUMBER_OF_COLUMNS_FOR_VEHICLE
+        )
+    );
+    console.log("numberOfRows", numberOfRows);
+    return ",,,,,,,,,,,,,";
+};
