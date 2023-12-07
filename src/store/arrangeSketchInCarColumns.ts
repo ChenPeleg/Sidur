5ca193c827126b69187f874d6c950abdf76ce119
Chen Peleg <chenpeleg@gmail.com>
2023-12-07 12:00:16 +0200

    added strings 2

--- src/store/arrangeSketchInCarColumns.ts
+++ src/store/arrangeSketchInCarColumns.ts
@@ -11,6 +11,7 @@ type VehicleSketchColumn = {
 
 const NUMBER_OF_ROWS_FOR_HEADER = 2;
 const NUMBER_OF_COLUMNS_FOR_VEHICLE = 3;
+const SEPERATOR = ",";
 
 const calculateNumberOfRows = (sketchObj: SketchModel): number => {
     const drivesPerVehicle = sketchObj.vehicleSchedules.map(
@@ -27,24 +28,56 @@ const vehicleSketchModelToVehicleSketchColumn = (
     vehicles: VehicleModel[],
     numberOfColumns: number
 ): VehicleSketchColumn => {
+    console.log(vehicles, vehicleSchedule);
     return {
         vehicleSchedule,
         vehicleName:
-            vehicles.find((v) => v.id === vehicleSchedule.id)?.vehicleName ||
-            "",
+            vehicles.find((v) => v.id === vehicleSchedule.VehicleId)
+                ?.vehicleName || "",
         numberOfColumns: numberOfColumns,
         position: 0,
     };
 };
 
 const buildCSVRow = (
-    vColumn: VehicleSketchColumn,
+    vColumn: VehicleSketchColumn[],
     rowNumber: number
 ): string => {
-    if (rowNumber === 0) {
-        return vColumn.vehicleName + ",,,";
-    }
-    return "";
+    const buildViehiclePartOfRow = (
+        vColumn: VehicleSketchColumn,
+        rowNumber: number
+    ): string => {
+        if (rowNumber === 0) {
+            return (
+                vColumn.vehicleName +
+                SEPERATOR.repeat(NUMBER_OF_COLUMNS_FOR_VEHICLE)
+            );
+        } else if (rowNumber === 1) {
+            return SEPERATOR + SEPERATOR + SEPERATOR;
+        } else {
+            const drive =
+                vColumn.vehicleSchedule.drives[
+                    rowNumber - NUMBER_OF_ROWS_FOR_HEADER
+                ];
+
+            if (!drive) {
+                return SEPERATOR.repeat(NUMBER_OF_COLUMNS_FOR_VEHICLE);
+            }
+            return (
+                drive.startHour +
+                " - " +
+                drive.finishHour +
+                SEPERATOR +
+                SEPERATOR +
+                drive.description +
+                SEPERATOR
+            );
+        }
+        return SEPERATOR.repeat(NUMBER_OF_COLUMNS_FOR_VEHICLE);
+    };
+    return vColumn
+        .map((vc) => buildViehiclePartOfRow(vc, rowNumber))
+        .join(SEPERATOR);
 };
 export const arrangeSketchInCarColumns = (
     sketchObj: SketchModel,
@@ -59,6 +92,9 @@ export const arrangeSketchInCarColumns = (
             NUMBER_OF_COLUMNS_FOR_VEHICLE
         )
     );
-    console.log("numberOfRows", numberOfRows);
-    return ",,,,,,,,,,,,,";
+    const csvRows = [...new Array(numberOfRows)].map((r, i) =>
+        buildCSVRow(vehicleSketchColumns, i)
+    );
+
+    return csvRows.join("\n");
 };
