bfc0ff02794f49a0b99e643b4e5dc74c11891603
Chen Peleg <chenpeleg@gmail.com>
2026-01-29 19:39:14 +0200

    chore: update type definitions for React and React DOM in package.json and package-lock.json

--- src/index.tsx
+++ src/index.tsx
@@ -13,14 +13,3 @@ createRoot(document.getElementById("root")!).render(
         </Root>
     </StrictMode>
 );
-
-// const root = ReactDOM.createRoot(
-//     document.getElementById("root") as HTMLElement
-// );
-// root.render(
-//     <React.StrictMode>
-//         <Root>
-//             <App />
-//         </Root>
-//     </React.StrictMode>
-// );
