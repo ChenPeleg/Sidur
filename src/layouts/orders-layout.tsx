117fd06a61529a9c15c457d35be5c427408301e0
Chen Peleg <chenpeleg@gmail.com>
2026-03-19 11:17:01 +0200

    chore: update moduleResolution in tsconfig.json to use bundler mode

--- src/layouts/orders-layout.tsx
+++ src/layouts/orders-layout.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import {Orders} from '../components/Orders/orders';
-import {Vehicles} from '../components/Vehicles/vehicles';
-import {LocationGroupSelect} from '../components/LocationGroupSelect/location-group-select';
+import React from "react";
+import { Orders } from "../components/Orders/orders";
+import { Vehicles } from "../components/Vehicles/vehicles";
+import { LocationGroupSelect } from "../components/LocationGroupSelect/location-group-select";
 
-
-export const OrdersLayout = () => (<>
-        <LocationGroupSelect/>
-        <Vehicles/>
-        <Orders/>
-    </>
-
-)
+export const OrdersLayout = () => (
+    <div className={"flex flex-col max-w-screen"}>
+        <LocationGroupSelect />
+        <Vehicles />
+        <Orders />
+    </div>
+);
