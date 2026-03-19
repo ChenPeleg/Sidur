4be37dd917c3f5f0d97e6585e395e79761c50661
Chen Peleg <chenpeleg@gmail.com>
2026-03-19 11:18:47 +0200

    chore: update moduleResolution in tsconfig.json to use bundler mode

--- src/components/Vehicles/vehicles.tsx
+++ src/components/Vehicles/vehicles.tsx
@@ -1,52 +1,55 @@
-import React, {useState} from 'react'
-import {translations} from '../../services/translations';
-import {SxProps} from '@mui/system';
-import {useDispatch, useSelector} from 'react-redux';
-import {ActionsTypes} from '../../store/types.actions';
-import {VehicleModel} from '../../models/Vehicle.model';
-import {Add, TimeToLeave} from '@mui/icons-material';
-import {Badge, Box, Button} from '@mui/material';
-import {VehicleEditDialog} from '../Dialogs/vehicle-edit-dialog';
-import {defaultVehicleValues} from '../../store/store.types';
-
+import React, { useState } from "react";
+import { translations } from "../../services/translations";
+import { SxProps } from "@mui/system";
+import { useDispatch, useSelector } from "react-redux";
+import { ActionsTypes } from "../../store/types.actions";
+import { VehicleModel } from "../../models/Vehicle.model";
+import { Add, TimeToLeave } from "@mui/icons-material";
+import { Badge, Box, Button } from "@mui/material";
+import { VehicleEditDialog } from "../Dialogs/vehicle-edit-dialog";
+import { defaultVehicleValues } from "../../store/store.types";
 
 export const Vehicles = () => {
     const [vehicleEditOpen, setVehicleEditOpen] = useState(false);
     const [vehicleClicked, setVehicleClicked] = useState(null);
     const dispatch = useDispatch();
-    const vehicles = useSelector((state: { vehicles: VehicleModel[] }) => state.vehicles || []);
-   
-    const vehicleButtonDesign: { boxSX: SxProps, variant: 'text' | 'outlined' | 'contained' } = {
+    const vehicles = useSelector(
+        (state: { vehicles: VehicleModel[] }) => state.vehicles || []
+    );
+
+    const vehicleButtonDesign: {
+        boxSX: SxProps;
+        variant: "text" | "outlined" | "contained";
+    } = {
         boxSX: {
-            display: 'flex',
-            margin: '0.5em',
-            mt: '0',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center'
+            display: "flex",
+            margin: "0.5em",
+            mt: "0",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
         },
-        variant: 'contained'
-    }
+        variant: "contained",
+    };
     const handleVehicleEditClose = (value: VehicleModel | null) => {
         setVehicleEditOpen(false);
 
         if (value) {
-            if (value.id === '0') {
+            if (value.id === "0") {
                 dispatch({
                     type: ActionsTypes.NEW_VEHICLE,
                     payload: {
-                        value
-                    }
-                })
+                        value,
+                    },
+                });
             } else {
                 dispatch({
                     type: ActionsTypes.UPDATE_VEHICLE,
                     payload: {
-                        value
-                    }
-                })
+                        value,
+                    },
+                });
             }
-
         }
     };
     const handleVehicleDelete = (id: string | null) => {
@@ -55,72 +58,93 @@ export const Vehicles = () => {
             dispatch({
                 type: ActionsTypes.DELETE_VEHICLE,
                 payload: {
-
-                    id
-                }
-            })
+                    id,
+                },
+            });
         }
     };
     const vehicleClickHandler = (event: any, vehicleId: string) => {
-        const foundVehicle = vehicles.find(v => v.id === vehicleId);
-        const vehicle: any = vehicleId !== '0' && foundVehicle ? foundVehicle : {
-            ...defaultVehicleValues,
-            id: '0'
-        };
+        const foundVehicle = vehicles.find((v) => v.id === vehicleId);
+        const vehicle: any =
+            vehicleId !== "0" && foundVehicle
+                ? foundVehicle
+                : {
+                      ...defaultVehicleValues,
+                      id: "0",
+                  };
         setVehicleClicked(vehicle);
-        setVehicleEditOpen(true)
-
-    }
+        setVehicleEditOpen(true);
+    };
 
     return (
         <Box>
-            <Box sx={{
-                fontSize: 'large',
-                // height: '150px',
-                mb: '1em',
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'start',
-                justifyContent: 'space-around'
-            }}>
-
+            <Box
+                sx={{
+                    fontSize: "large",
+                    // height: '150px',
+                    mb: "1em",
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    alignItems: "start",
+                    justifyContent: "start",
+                }}
+            >
                 {vehicles.map((v: VehicleModel, index: number) => (
-                    <Box key={index} sx={{
-                        ...vehicleButtonDesign
-                            .boxSX
-                    }}>
-                        <Button variant={vehicleButtonDesign
-                            .variant} onClick={(event) => {
-                            vehicleClickHandler(event, v.id)
-                        }}>
-                            <Badge color={'warning'} invisible={v.seats === '5'} badgeContent={7}>
-                                <TimeToLeave sx={{
-                                    width: '1.2em',
-                                    height: '1.2em'
-                                }}/>
+                    <Box
+                        key={index}
+                        sx={{
+                            ...vehicleButtonDesign.boxSX,
+                        }}
+                    >
+                        <Button
+                            variant={vehicleButtonDesign.variant}
+                            onClick={(event) => {
+                                vehicleClickHandler(event, v.id);
+                            }}
+                        >
+                            <Badge
+                                color={"warning"}
+                                invisible={v.seats === "5"}
+                                badgeContent={7}
+                            >
+                                <TimeToLeave
+                                    sx={{
+                                        width: "1.2em",
+                                        height: "1.2em",
+                                    }}
+                                />
                             </Badge>
 
-                            {v.vehicleName}</Button>
-                    </Box>))}
-                <Box sx={{
-                    ...vehicleButtonDesign
-                        .boxSX
-                }}>
-                    <Button variant={vehicleButtonDesign
-                        .variant} onClick={(event) => vehicleClickHandler(event, '0')}>
-                        <Add sx={{
-                            width: '1.2em',
-                            height: '1.2em'
-                        }}/> {translations.AddVehicles} </Button>
-
+                            {v.vehicleName}
+                        </Button>
+                    </Box>
+                ))}
+                <Box
+                    sx={{
+                        ...vehicleButtonDesign.boxSX,
+                    }}
+                >
+                    <Button
+                        variant={vehicleButtonDesign.variant}
+                        onClick={(event) => vehicleClickHandler(event, "0")}
+                    >
+                        <Add
+                            sx={{
+                                width: "1.2em",
+                                height: "1.2em",
+                            }}
+                        />{" "}
+                        {translations.AddVehicles}{" "}
+                    </Button>
                 </Box>
             </Box>
-            <VehicleEditDialog vehicleData={vehicleClicked} open={vehicleEditOpen} onClose={handleVehicleEditClose}
-                               onDelete={handleVehicleDelete}/>
-
+            <VehicleEditDialog
+                vehicleData={vehicleClicked}
+                open={vehicleEditOpen}
+                onClose={handleVehicleEditClose}
+                onDelete={handleVehicleDelete}
+            />
         </Box>
-
-
-    )
-
-}
+    );
+};
